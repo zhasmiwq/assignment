48a97f52d7457160071121f23f0510881b315ef5
Zharkyn077 <zhenisuly_zh@icloud.com>
2026-01-24 13:17:26 +0500

    Zharkyn

--- oop2026zz/src/edu/aitu/oop3/data/PostgresDB.java
+++
@@ -1,4 +0,0 @@
-package edu.aitu.oop3.data;
-
-public class PostgresDB {
-}
--- oop2026zz/src/edu/aitu/oop3/services/CourseService.java
+++ oop2026zz/src/edu/aitu/oop3/services/CourseService.java
@@ -1,4 +1,38 @@
 package edu.aitu.oop3.services;
 
+import oop2026_groupIT25XX_online_learning.entities.Course;
+import oop2026_groupIT25XX_online_learning.exceptions.NotFoundException;
+import oop2026_groupIT25XX_online_learning.repositories.CourseRepository;
+import oop2026_groupIT25XX_online_learning.repositories.UserRepository;
+
+import java.util.List;
+
 public class CourseService {
+    private final CourseRepository courseRepo;
+    private final UserRepository userRepo;
+
+    public CourseService(CourseRepository courseRepo, UserRepository userRepo) {
+        this.courseRepo = courseRepo;
+        this.userRepo = userRepo;
+    }
+
+    public Course createCourse(String title, String description, long teacherId) {
+        if (title == null || title.trim().length() < 3)
+            throw new IllegalArgumentException("Title must be at least 3 chars");
+
+        var teacherOpt = userRepo.findById(teacherId);
+        if (teacherOpt.isEmpty()) throw new NotFoundException("Teacher not found: id=" + teacherId);
+        if (!"TEACHER".equals(teacherOpt.get().getRole()))
+            throw new IllegalArgumentException("This user is not TEACHER");
+
+        return courseRepo.create(new Course(0, title.trim(), description, teacherId));
+    }
+
+    public List<Course> listCourses() {
+        return courseRepo.findAll();
+    }
+
+    public Course getCourse(long id) {
+        return courseRepo.findById(id).orElseThrow(() -> new NotFoundException("Course not found: id=" + id));
+    }
 }
