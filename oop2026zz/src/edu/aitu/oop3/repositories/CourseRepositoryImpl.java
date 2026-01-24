1103ac4c7bcfc8ac37c5269c141f74b7de8698e3
zhasmiwq <zhasmiwq@gmail.com>
2026-01-24 13:49:45 +0500

    Zhasmin

--- oop2026zz/src/edu/aitu/oop3/repositories/CourseRepositoryImpl.java
+++ oop2026zz/src/edu/aitu/oop3/repositories/CourseRepositoryImpl.java
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class CourseRepositoryImpl extends CourseRepository {
+public class CourseRepositoryImpl {
     private final IDB db;
 
     public CourseRepositoryImpl(IDB db) {
--- oop2026zz/src/edu/aitu/oop3/repositories/LessonRepositoryImpl.java
+++ oop2026zz/src/edu/aitu/oop3/repositories/LessonRepositoryImpl.java
@@ -10,7 +10,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class LessonRepositoryImpl implements LessonRepository {
+public class LessonRepositoryImpl {
     private final IDB db;
 
     public LessonRepositoryImpl(IDB db) {
