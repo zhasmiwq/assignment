395b6d39dd642407cf46a48477ba8b6f48da3752
Zharkyn077 <zhenisuly_zh@icloud.com>
2026-01-24 13:57:10 +0500

    Zharkyn

--- oop2026zz/src/edu/aitu/oop3/repositories/CourseRepository.java
+++ oop2026zz/src/edu/aitu/oop3/repositories/CourseRepository.java
@@ -1,4 +1,13 @@
 package edu.aitu.oop3.repositories;
 
-public class CourseRepository {
+import oop2026_groupIT25XX_online_learning.entities.Course;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CourseRepository {
+    Course create(Course course);
+    Optional<Course> findById(long id);
+    List<Course> findAll();
+    List<Course> findByTeacher(long teacherId);
 }
