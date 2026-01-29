a9df7419287697109820a147152becf828dea860
zhasmiwq <zhasmiwq@gmail.com>
2026-01-29 21:38:41 +0500

    Zhasmin

--- oop2026zz/src/edu/aitu/oop3/db/DataConnection.java
+++ oop2026zz/src/edu/aitu/oop3/db/DataConnection.java
@@ -1,18 +1,33 @@
 package edu.aitu.oop3.db;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-    public class DataConnection {
-        private static final String URL =
-                "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
-        private static final String USER = "postgres.gmnhoitoccbzuveofkns";
-        private static final String PASSWORD = "askarova0910";// ← DATABASE PASSWORD
-        private DataConnection() {
-            // no instances
-        }
-        public static Connection getConnection() throws SQLException {
-            return DriverManager.getConnection(URL, USER, PASSWORD);
+import java.util.Properties;
+public class DataConnection {
+    private static final String URL =
+            "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
+    private static final String USER = "postgres.gmnhoitoccbzuveofkns";
+    private static final String PASSWORD = loadPassword();
+    private static String loadPassword() {
+        Properties props = new Properties();
+        try (InputStream input = new FileInputStream("config.properties")) {
+            props.load(input);
+            String value = props.getProperty("DB_PASSWORD");
+            if (value == null || value.isBlank()) {
+                throw new RuntimeException("DB_PASSWORD is not set in config.properties");
+            }
+            return value;
+        } catch (IOException e) {
+            throw new RuntimeException("Cannot load DB_PASSWORD from config.properties", e);
         }
     }
-
+    private DataConnection() {
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
