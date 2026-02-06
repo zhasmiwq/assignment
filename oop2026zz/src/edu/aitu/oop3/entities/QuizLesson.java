8d86250e15c920cf072b70487030350945cc16bb
zhasmiwq <zhasmiwq@gmail.com>
2026-02-06 23:12:16 +0500

    zh

--- oop2026zz/src/edu/aitu/oop3/entities/QuizLesson.java
+++ oop2026zz/src/edu/aitu/oop3/entities/QuizLesson.java
@@ -4,7 +4,7 @@ public final class QuizLesson extends Lesson {
     private final int questionsCount;
 
     public QuizLesson(int id, int courseId, String title, int questionsCount) {
-        super(id, courseId, title, LessonType.QUIZ);
+        super(id, courseId, title, String.valueOf(LessonType.QUIZ));
         this.questionsCount = questionsCount;
     }
 
